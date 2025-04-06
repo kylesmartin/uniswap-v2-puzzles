1e856f5c36e2c7a11c2a0989aef288025ad8e0e4
Kyle Martin <kyle.martin@smartcontract.com>
2025-04-06 14:33:00 -0400

    Twap

--- src/Twap.sol
+++ src/Twap.sol
@@ -38,10 +38,18 @@ contract Twap {
     //**       ONE HOUR TWAP START      **//
     function first1HourSnapShot() public {
         // your code here
+        (,, first1HourSnapShot_TimeStamp) = pool.getReserves();
+        first1HourSnapShot_Price0Cumulative = pool.price0CumulativeLast();
     }
 
     function second1HourSnapShot() public returns (uint224 oneHourTwap) {
-        // your code here
+        (,, second1HourSnapShot_TimeStamp) = pool.getReserves();
+        second1HourSnapShot_Price0Cumulative = pool.price0CumulativeLast();
+
+        uint256 timeElapsed = second1HourSnapShot_TimeStamp - first1HourSnapShot_TimeStamp;
+        oneHourTwap = uint224(
+            (second1HourSnapShot_Price0Cumulative - first1HourSnapShot_Price0Cumulative) / timeElapsed
+        );
 
         return oneHourTwap;
     }
@@ -50,10 +58,19 @@ contract Twap {
     //**       ONE DAY TWAP START      **//
     function first1DaySnapShot() public {
         // your code here
+        (,, first1DaySnapShot_TimeStamp) = pool.getReserves();
+        first1DaySnapShot_Price0Cumulative = pool.price0CumulativeLast();
     }
 
     function second1DaySnapShot() public returns (uint224 oneDayTwap) {
         // your code here
+        (,, second1DaySnapShot_TimeStamp) = pool.getReserves();
+        second1DaySnapShot_Price0Cumulative = pool.price0CumulativeLast();
+
+        uint256 timeElapsed = second1DaySnapShot_TimeStamp - first1DaySnapShot_TimeStamp;
+        oneDayTwap = uint224(
+            (second1DaySnapShot_Price0Cumulative - first1DaySnapShot_Price0Cumulative) / timeElapsed
+        );
 
         return (oneDayTwap);
     }
